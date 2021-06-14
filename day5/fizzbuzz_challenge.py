89b4a12e94417aab5c9e5d995fa779f4e53f1e85
simran0963 <simranmehta0963@gmail.com>
2021-06-14 17:41:38 +0530

    commit changes

---
+++ day5/fizzbuzz_challenge.py
@@ -0,0 +1,13 @@
+for number in range(1, 101):
+	if number % 15 == 0 :
+		print("Fizz Buzz")
+
+	elif number % 3 == 0 :
+		print("Fizz")
+
+	elif number % 5 == 0 :
+		print("Buzz")
+
+	
+	else :
+		print(number)
