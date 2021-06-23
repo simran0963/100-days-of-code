c4d009dcf4534574bc049a5572ae4fe555471ba6
simran0963 <simranmehta0963@gmail.com>
2021-06-23 19:58:17 +0530

    d1

---
+++ d1/print.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this: ")
+print("print('what to print')")
+
+
+print("Hello " + "World")
+print("Hello" + " " + "World")
+
+
+
+
+
+
+
+
+
+
+
