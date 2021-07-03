e6cce75435260d45d7fd1087d02a2a0a4f3d2d73
simran0963 <simranmehta0963@gmail.com>
2021-07-03 19:47:47 +0530

    d2

--- day2/challenge.py
+++ day2/challenge.py
@@ -1,6 +1,8 @@
+#take the age as input
 age = input("What is your current age?\n")
 age = int(age)
 
+#Taking 90 as the average age calculate the number of days, weeks and minths left
 days = (365 * 90) - (365 * age)
 weeks = (52 * 90) - (52 * age)
 months = (12 * 90) - (12 * age)
