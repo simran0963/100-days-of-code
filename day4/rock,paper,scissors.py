7231b3edfc9b4aa852efcbe33eacf6e42ba8f8f5
simran0963 <simranmehta0963@gmail.com>
2021-07-18 19:23:28 +0530

    d4

--- day4/rock,paper,scissors.py
+++ day4/rock,paper,scissors.py
@@ -1,6 +1,11 @@
 import random
+from art import scissors, rock, paper
+import os
 
+def clear():
+	os.system('cls')
 
+clear()
 choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
 if choose > 3 or choose < 0 :
 	print("You chose an invalid number, you lose.")
