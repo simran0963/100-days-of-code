967c6eeddac3e23e0eae7bdf690c56c3b1535f04
simran0963 <simranmehta0963@gmail.com>
2021-07-30 23:05:25 +0530

    d15

---
+++ day15/coffee_machine_project.py
@@ -0,0 +1,15 @@
+#TODO: 1. Print the report of all coffee machine resources.
+#TODO: 2. Check that the resources are sufficient to make the drink order.
+#TODO: 3. Process coins.
+#TODO: 4. Check if the transaction was successful.
+#TODO: 5. Make coffee.
+
+from CoffeeMachine import MENU, resources
+import os
+
+def clear():
+	os.system('cls')
+
+clear()
+
+print(resources)
