26ae291b4abc1015cc8b516da19efb716f0c9916
simran0963 <simranmehta0963@gmail.com>
2021-08-05 23:29:25 +0530

    d6

--- day6/defining-and-calling.py
+++ day6/defining-and-calling.py
@@ -1,4 +1,4 @@
-#First define your own function and insert some code within it
+#First define your own function and insert some code within it, you can also add some parameters to the function you want to change or use within it/
 def my_function() :
 	print("Hello")
 	num_char = len("Hello")
