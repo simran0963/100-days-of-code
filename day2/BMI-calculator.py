9179ee639dfa1cdcf6573c77e21a9c7d2041c082
simran0963 <simranmehta0963@gmail.com>
2021-08-09 16:38:56 +0530

    d2

--- day2/BMI-calculator.py
+++ day2/BMI-calculator.py
@@ -6,8 +6,7 @@ height = input("enter your height in m ")
 height = float(height)
 
 bmi = weight/(height ** 2)
-bmi_as_int = int(bmi)
 
 # print("Your BMI is " + str(result))
 # print(bmi_as_int)
-print(round(bmi_as_int))
+print(int(round(bmi)))
