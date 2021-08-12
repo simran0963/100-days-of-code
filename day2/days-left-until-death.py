a61bb3108f8eae483aeb839ad3268e6f3119a8fb
simran0963 <simranmehta0963@gmail.com>
2021-08-12 20:18:58 +0530

    d2

--- day2/days-left-until-death.py
+++ day2/days-left-until-death.py
@@ -8,3 +8,12 @@ weeks = (52 * 90) - (52 * age)
 months = (12 * 90) - (12 * age)
 result = f"You have {days} days, {weeks} weeks, and {months} months left."
 print(result)
+
+# TO calculate exact weeks try the code below.
+
+# age = int(input("Enter your age here: "))
+# years = 90 - age
+# days = years * 365
+# weeks = int(days / 7)
+# months = years * 12
+# print(f"You have {days} days, {weeks} weeks and {months} months left.")
